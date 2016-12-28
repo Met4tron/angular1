47bb839e5a00b0e25a1ceeabae8543a273550204
Yuri Gomes <desenvolvedoryuri@gmail.com>
2016-12-28 12:41:28 -0200

    Consumindo uma API
    Introdução ao consumo de uma API e a diretiva ng-repeat

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,8 +1,20 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-  $scope.foto = {
-    titulo: "Leão",
-    url:"http://www.fundosanimais.com/Minis/leoes.jpg"
-  }
+  $scope.fotos = []
+
+  $http.get('v1/fotos')
+  .sucess(function(fotos){
+    $scope.fotos = fotos;
+  })
+  .error(function(error){
+    console.log(error);
+  })
+
+  /*var promise = $http.get('v1/fotos');
+  promise.then(function(retorno){
+    $scope.fotos = retorno.data;
+  }).catch(function(err){
+    console.log(err);
+  });*/
 
 });
